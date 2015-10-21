fd425773d31801df0aafad172f4732ef253c1abd
KreepN <jamourj@gmail.com>
2015-10-21 13:59:35 -0500

    Project Setup

--- LMD/LMD/Properties/AssemblyInfo.cs
+++ LMD/LMD/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ASP.NET_MVC5_Bootstrap3_3_4_LESS")]
+[assembly: AssemblyTitle("LMD")]
 [assembly: AssemblyDescription("Default ASP.NET MVC5 Template with Bootstrap 3.3.4 LESS")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Krzysztof Osowicki")]
-[assembly: AssemblyProduct("ASP.NET_MVC5_Bootstrap3_3_1_LESS")]
+[assembly: AssemblyProduct("LMD")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
