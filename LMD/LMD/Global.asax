6be3c4d10c7dbb4b3c6d6342da6182adf4271fff
KreepN <jamourj@gmail.com>
2015-10-21 12:11:07 -0500

    Proj Creation

---
+++ LMD/LMD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LMD.MvcApplication" Language="C#" %>
---
+++ LMD/LMD/Startup.cs
@@ -0,0 +1,15 @@
+﻿using LMD;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace LMD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
